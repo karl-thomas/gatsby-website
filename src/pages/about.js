42e9673b554100074a629aa2bf456e4c1c524088
karl-thomas <karl.thomas925@gmail.com>
2019-09-05 19:10:04 -0500

    build my fundament

---
+++ src/pages/about.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Link} from 'gatsby';
+export default () => 
+  <>
+   <h1>About</h1>
+   <Link to="/"> &larr; back to home</Link>
+  </>;
---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import {Link} from 'gatsby';
+
+export default () => <div>
+  Hello Frontend Masters!
+  nice <Link to="/about/"> go to about &rarr;</Link>
+  </div>;
