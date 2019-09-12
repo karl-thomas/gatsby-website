497ed1e8ee16772582e08330a9216dbd303ff6fb
karl-thomas <karl.thomas925@gmail.com>
2019-09-12 10:46:19 -0500

    add mdx and a little wave component to play around with.

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,5 +3,16 @@ module.exports = {
     title: 'Starting Blog',
     description: 'a nice blog to start with',
   },
-  plugins: ['gatsby-plugin-emotion', 'gatsby-plugin-react-helmet']
+  plugins: [
+    'gatsby-plugin-emotion',
+    'gatsby-plugin-react-helmet',
+    {
+      resolve: 'gatsby-plugin-mdx',
+      options: {
+        defaultLayouts: {
+          default: require.resolve('./src/components/layout')
+        }
+      }
+    }
+  ]
 }
