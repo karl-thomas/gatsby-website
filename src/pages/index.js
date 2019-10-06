d08034e617be73e3c8a611fb0e8804ba4aba9e98
karl-thomas <karl.thomas925@gmail.com>
2019-10-06 14:54:43 -0500

    load json preview of posts on the page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,21 @@
 import React from 'react';
 import {Link} from 'gatsby';
 import Layout from '../components/layout';
+import usePosts from '../hooks/use-posts';
 
-export default () => 
-  <Layout>
-    Hello Frontend Masters!
-    nice <Link to="/about/"> go to about &rarr;</Link>
-  </Layout>;
+export default () => {
+  const posts = usePosts();
+  
+  return (
+    <Layout>
+      Hello Frontend Masters!
+      nice <Link to="/about/"> go to about &rarr;</Link>
+
+      <h2>Read my blog</h2>
+      {posts.map(post => (
+        <pre>{JSON.stringify(post, null, 4)}</pre>
+      ))}
+    </Layout>
+  );
+}
+  
