3318df135a81d456273a4d611534dae014fb0bec
karl-thomas <karl.thomas925@gmail.com>
2019-10-06 16:02:43 -0500

    create components for the blog posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,18 +2,20 @@ import React from 'react';
 import {Link} from 'gatsby';
 import Layout from '../components/layout';
 import usePosts from '../hooks/use-posts';
+import PostPreview from '../components/post-preview';
 
 export default () => {
   const posts = usePosts();
   
   return (
     <Layout>
-      Hello Frontend Masters!
+      Hello
+      
       nice <Link to="/about/"> go to about &rarr;</Link>
 
       <h2>Read my blog</h2>
       {posts.map(post => (
-        <pre>{JSON.stringify(post, null, 4)}</pre>
+        <PostPreview key={post.slug} post={post}/>
       ))}
     </Layout>
   );
