2d08df4d9cf811224b4392b2b3603b8769a178e5
karl-thomas <karl.thomas925@gmail.com>
2019-10-16 20:50:51 -0500

    remove some comments and a unused function

--- src/components/canvas.js
+++ src/components/canvas.js
@@ -8,11 +8,12 @@ const Canvas = () => {
   const width = window.innerWidth;
   const height = window.innerHeight * 0.4;
 
-  const coinFlip = () => (Math.floor(Math.random() * 2) == 0);
-
   const sketch = ({ width, height }) => {
     // Padding around edges
     const margin = width * marginConst;
+    const palette = random.pick([
+      ['#84BC92', '#442F5E', '#EABA8A', '#191011', '#FAF9FC']
+    ]);
 
     const canvasContainer = document.getElementById('canvas-container');
     const canvas = document.createElement('canvas');
@@ -20,16 +21,8 @@ const Canvas = () => {
     canvas.setAttribute('height', height);
     canvas.setAttribute('id', 'canvas');
     canvasContainer.appendChild(canvas);
+
     const context = canvas.getContext("2d");
-    const palette = random.pick([
-      ['#84BC92', '#442F5E', '#EABA8A', '#191011', '#FAF9FC']
-    ]);
-    // ["#c9df8a", "#234d20"],
-    // ["#fcb653", "#cee879", "#5cacc4", "#ff5254"],
-    // ["#c4ac30", "#1e8c93", "#d74f33", "#dbd8a2"],
-    // ["#66b6ab", "#5b7c8d", "#4f2958"],
-    // ["#c97ba5", "#ff7a24", "#f76d75", "#ff6d54"],
-    // ["#dae98a", "#e17572", "#ce1446", "#2b0b16"]
     const background = 'white';
 
     // Create a grid of points (in pixel space) within the margin bounds
@@ -93,41 +86,38 @@ const Canvas = () => {
 
     // Sort/layer the shapes according to their average Y position
     shapes.sort((a, b) => a.y - b.y);
-    // 191011
-    // FAF9FC
+
     // Now render
     return () => {
-      // Make sure our alpha is back to 1.0 before
-      // we draw our background color
       context.globalAlpha = 1;
       context.fillStyle = background;
       context.fillRect(0, 0, width, height);
-
+      
       shapes.forEach(({ lineWidth, path, color }) => {
-        context.beginPath();
-        let [lastX, lastY] = random.pick(path);
-        const randomNumber = random.range(20, 80)
-        // let lastY = , lastX = 0;
-        path.forEach(([x, y]) => {
-          // context.arc(x, y, 40, 0);
-          context.bezierCurveTo(lastX, lastY + randomNumber, x, lastY - randomNumber * 0.5, x, y);
-          lastX = x;
-          lastY = y;
-        });
-        context.closePath();
+          context.beginPath();
+          let [lastX, lastY] = random.pick(path);
+          const randomNumber = random.range(20, 80)
+          // let lastY = , lastX = 0;
+          path.forEach(([x, y]) => {
+            // context.arc(x, y, 40, 0);
+            context.bezierCurveTo(lastX, lastY + randomNumber, x, lastY - randomNumber * 0.5, x, y);
+            lastX = x;
+            lastY = y;
+          });
+          context.closePath();
 
-        // Draw the trapezoid with a specific colour
-        // context.lineWidth = (window.width / 100) * 5 > 30 ? 30 : (window.width / 100) * 5;
-        context.lineWidth = 5;
-        context.globalAlpha = 0.85;
-        context.fillStyle = color;
-        context.fill();
+          // Draw the trapezoid with a specific colour
+          // context.lineWidth = (window.width / 100) * 5 > 30 ? 30 : (window.width / 100) * 5;
+          context.lineWidth = 5;
+          context.globalAlpha = 0.85;
+          context.fillStyle = color;
+          context.fill();
 
-        // Outline at full opacity
-        context.lineJoin = context.lineCap = 'round';
-        context.strokeStyle = background;
-        context.globalAlpha = 1;
-        context.stroke();
+          // Outline at full opacity
+          context.lineJoin = context.lineCap = 'round';
+          context.strokeStyle = background;
+          context.globalAlpha = 1;
+          context.stroke();
       });
     };
   };
