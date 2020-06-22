f962166d73775194fe1ddc536bac7d16771b50d9
karl-thomas <karl.thomas925@gmail.com>
2020-06-22 16:30:35 -0500

    update filename

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,7 +18,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-transition-link',
       options: {
-        layout: require.resolve('./src/components/layout'),
+        layout: require.resolve('./src/components/Layout'),
       },
     },
     {
